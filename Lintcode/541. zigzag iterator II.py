ae040ec5f96ac9c688d84741fe00f3c6c7150800
Amber G <amber.g0330@gmail.com>
2021-08-13 15:57:51 -0700

    Update 541. zigzag iterator II.py

--- Lintcode/541. zigzag iterator II.py
+++ Lintcode/541. zigzag iterator II.py
@@ -1,3 +1,27 @@
+Description
+Follow up Zigzag Iterator: What if you are given k 1d vectors? How well can your code be extended to such cases? The "Zigzag" order is not clearly defined and is ambiguous for k > 2 cases. If "Zigzag" does not look right to you, replace "Zigzag" with "Cyclic".
+
+Example
+Example1
+
+Input: k = 3
+vecs = [
+    [1,2,3],
+    [4,5,6,7],
+    [8,9],
+]
+Output: [1,4,8,2,5,9,3,6,7]
+Example2
+
+Input: k = 3
+vecs = [
+    [1,1,1]
+    [2,2,2]
+    [3,3,3]
+]
+Output: [1,2,3,1,2,3,1,2,3]
+    
+
 # Method 1. 双指针解法
 class ZigzagIterator2:
     """
