eeea95d90c31490133622b655f2b84d5c88929b7
Amber G <amber.g0330@gmail.com>
2021-08-21 15:28:41 -0700

    Add files via upload

--- Lintcode/421. simplify path.py
+++ Lintcode/421. simplify path.py
@@ -1,3 +1,26 @@
+# Description
+# Given an absolute path for a file (Unix-style), simplify it.
+
+# In a UNIX-style file system, a period . refers to the current directory. Furthermore, a double period .. moves the directory up a level.
+
+# The result must always begin with /, and there must be only a single / between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the result must be the shortest string representing the absolute path.
+
+# Did you consider the case where path is "/../"?
+
+# In this case, you should return "/".
+
+# Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+
+# In this case, you should ignore redundant slashes and return "/home/foo".
+
+# Example 2:
+
+# Input: "/a/./../../c/"
+# Output: "/c"
+# Explanation: "/" has no parent directory, so "/../" equals "/".
+
+
+
 class Solution: 
 
     def simplifyPath(self, path):
