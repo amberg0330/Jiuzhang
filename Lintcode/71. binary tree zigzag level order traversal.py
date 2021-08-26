45a4df48732a4f00d5a497a1e428be6272aba4fc
Amber G <amber.g0330@gmail.com>
2021-08-25 23:14:51 -0700

    Add files via upload

--- Lintcode/71. binary tree zigzag level order traversal.py
+++ Lintcode/71. binary tree zigzag level order traversal.py
@@ -1,3 +1,38 @@
+# Description
+# Given a binary tree, return the zigzag level order traversal of its nodes' values.
+# (ie, from left to right, then right to left for the next level and alternate between).
+#
+# Example
+# Example 1:
+#
+# Input:
+#
+# tree = {1,2,3}
+# Output:
+#
+# [[1],[3,2]]
+# Explanation:
+#
+#     1
+#    / \
+#   2   3
+# it will be serialized {1,2,3}
+# Example 2:
+#
+# Input:
+#
+# tree = {3,9,20,#,#,15,7}
+# Output:
+#
+# [[3],[20,9],[15,7]]
+# Explanation:
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# it will be serialized {3,9,20,#,#,15,7}
 """
 Definition of TreeNode:
 class TreeNode:
@@ -44,7 +79,7 @@ class Solution:
             is_left_to_right = not is_left_to_right
         return mylist
 
-# method 2: 用stack & queue来做
+# method 2: BFS + stack
 class Solution:
     def zigzagLevelOrder(self, root):
         mylist = []
